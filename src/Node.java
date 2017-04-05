507f3c86f004ee5440126843166fe7e15f104711
BT_1N3_14 <BT_1N3_14@BT_1N3_14.bktech.local>
2017-04-05 13:19:11 -0400

    Added Node and AStar search

--- src/Node.java
+++ src/Node.java
@@ -1,95 +1,87 @@
+//Jia Hua Zhu
+//3/10/2017
+//AP Java Node Class
+
 import java.util.ArrayList;
 
-public class Node 
-{
-	class Node implements Comparable<Node>{
-		
-	    private String name;
-	    private ArrayList<Node> children;
-	    private int distance = Integer.MAX_VALUE;
-	    private double cost = Integer.MAX_VALUE;
-	    private Node parent;
-	    private int owner;
-	    
-	    int index;
-	    int x;
-	    int y;
-	    
-	    public Node(String name, ArrayList<Node> children)
-	    {
-	        this.name = name;
-	        this.children = children;
-	    }
-	    public Node(String name, ArrayList<Node> children, int index)
-	    {
-	        this.name = name;
-	        this.children = children;
-	        this.index = index;
-	    }
-	    public Node(int x, int y)
-	    {
-	        this.name = "(" + x + "," + y + ")";
-	        this.x = x;
-	        this.y = y;
-	        this.owner = -1;
-	        }
+public class Node implements Comparable<Node> {
+	private String nodeName; 										
+	private Node prevCity;					 						
+	private int index; 												
+	private int distance = Integer.MAX_VALUE; 						
+	private ArrayList<Node> childrenNodes = new ArrayList<Node>();
+	private int x;
+	private int y;
+	private double cost; //in this simulation, all nodes have the same cost
 
-	    public ArrayList<Node> getChildren()
-	    {
-	        return this.children;
-	    }
+	public Node(String name, ArrayList<Node> nextNode) {
+		this.nodeName = name;
+		this.childrenNodes = nextNode;
+	}	
 
-	    public void setChildren(ArrayList<Node> children)
-	    {
-	        this.children = children;
-	    }
-	    
-	    public int getDistance()
-	    {
-	    	return this.distance;
-	    }
-	    public void setDistance(int dist) 
-	    {
-	    	this.distance = dist;
-	    }
-	    public Node getParent()
-	    {
-	    	return this.parent;
-	    }
-	    public void setParent(Node node)
-	    {
-	    	this.parent = node;
-	    }
+	public Node(String cityName, ArrayList<Node> nextCity, int index) {
+		this.nodeName = cityName;
+		this.childrenNodes = nextCity;
+		this.index = index;
+	}
+	
+	public Node(int row, int column) {
+		this.x = row;
+		this.y = column;
+		this.nodeName = ("(" + x + ", " + y + ")");
+	}
+	
+	public int x() {
+		return x;
+	}
+	
+	public int y() {
+		return y;
+	}
+	
+	public void setCost(double nodeCost) {
+		this.cost = nodeCost;
+	}
+	
+	public double cost() {
+		return this.cost;
+	}
+	
+	public void setChildren(ArrayList<Node> cities) {
+		this.childrenNodes = cities;
+	}
 
-	    
-	    public String toString()
-	    {
-	        return this.name;
-	    }
-	   
-	    public double getCost() {
-	    	return this.cost;
-	    }
-	    
-	    public void setCost(double cost)
-	    {
-	    	this.cost = cost;
-	    }
-	    public void setOwner(int owner)
-	    {
-	    	this.owner = owner;
-	    }
-	    public int getOwner()
-	    {
-	    	return this.owner;
-	    }
-	    
-	    
-	    public int compareTo(Node node)
-	    {
-			return Double.compare(this.cost, node.cost);
-	    }
+	public ArrayList<Node> getChildren() {
+		return childrenNodes;
+	}
+	
+	public void setDistance(int dist) {
+		this.distance = dist;
+	}
+	
+	public int getDistance() {
+		return distance;
+	}
+	
+	public String toString() {
+		return nodeName;
+	}
+	
+	public void setParent(Node lastCity) {
+		this.prevCity = lastCity;
+	}
+	
+	public Node getParent() {
+		return prevCity;
+	}
+	
+	public int getIndex() {
+		return index;
+	}
 
-
-
+	public int compareTo(Node o) {
+		if(this.cost > o.cost) return 1;
+		else if(this.cost < o.cost) return -1;
+		else return 0;
+	}
 }
